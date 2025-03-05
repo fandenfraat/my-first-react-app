865d9f6d02de8cf711890a666ede54e1216ff7c1
Fani Oktaf Laurisa <95343841+fandenfraat@users.noreply.github.com>
2025-03-05 13:09:01 +0700

    mengganti code appjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+  font-size: 20px;
+  color: red;
+`;
+const Section = styled.section`
+  padding: 40px;
+  background: papayawhip;
+  `;
+const Button = styled.button`
+  padding: 8px 12px;
+  height: 40px;
+  color: ${props => props.primary ? 'white' : 'blue'};
+  border: 1px solid blue;
+  background: ${props => props.primary ? 'blue' : 'white'};
+  border-radius: 4px;
+  margin-right: 10px;
+  `
+  const GreenButton = styled(Button)`
+    background: green;
+   
+    border-color:green;
+    `
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <>
+    <Title>This is Title H1</Title>
+    <Section>This is Section</Section><br/>
+    <Button>normal</Button>
+    <Button primary>Primary Button</Button>
+    <GreenButton primary>Green Button</GreenButton>
+    </>
   );
 }
 
